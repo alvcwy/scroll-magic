c2409e88b462fdafc91e67b394feb160fe5e3e48
alvcwy <93187830+alvcwy@users.noreply.github.com>
2023-01-16 19:13:27 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Scroll Magic
+
+This demo uses the Scroll Magic library to place a video in the background, allowing you to play the video by scrolling down the webpage. 
+
+### Why?
+- Allows for slow-mo playback
+- Can pause at an exact moment
+- Play the video in reverse by scrolling backwards
+
+This can be extended to recreate webpages like [Apple's AirPods animation](https://www.apple.com/airpods-3rd-generation/). 
---
+++ script.js
@@ -0,0 +1,73 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+//END SECTION
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+//Scenes
+let scene = new ScrollMagic.Scene({
+  duration: video.duration * 100 * 2.5, // For a 10 second video, this will give a duration of 2500
+  triggerElement: intro,
+  triggerHook: 0
+})
+  //.addIndicators()  // Shows start and end of scene when scrolling
+  .setPin(intro)
+  .addTo(controller);
+
+//Text Animation
+const textAnim = gsap.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+
+let scene2 = new ScrollMagic.Scene({
+  duration: 3000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .setTween(textAnim)
+  .addTo(controller);
+
+//Video Animation
+let accelamount = 0.3;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", e => {
+  scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+  delay += (scrollpos * 4 - delay) * accelamount;
+  //console.log(scrollpos, delay);
+
+  video.currentTime = delay;
+}, 40);
+console.log(video);
+
+
+
+// Change display text
+document.querySelector('input[name="display_text"]').addEventListener('keyup', function() {
+  document.getElementById('video_text').innerHTML = this.value;
+});
+
+// Change video source
+document.getElementById("bojji").addEventListener("click", vidsource_Bojji);
+function vidsource_Bojji() {
+  document.getElementById("video").setAttribute('src', 'static/bojji.mp4');
+};
+
+document.getElementById("corgi").addEventListener("click", vidsource_Corgi);
+function vidsource_Corgi() {
+  document.getElementById("video").setAttribute('src', 'static/corgi.mp4');
+};
+
+// Display uploaded video as source
+document.getElementById("upload-video-file").addEventListener("change", function() {
+  const video_file = this.files[0];
+  const media = URL.createObjectURL(video_file);
+  var video = document.getElementById("video");
+  video.src = media;
+});
